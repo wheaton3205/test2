4972015104f6ec276d16db79e5cdd89deb0050ab
wheaton3205 <wheaton3205@gmail.com>
2015-12-10 15:25:45 -0600

    deleted all from laptop

--- WoopGitProj/src/gitProj/Test.java
+++ WoopGitProj/src/gitProj/Test.java
@@ -3,12 +3,5 @@ package gitProj;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Yoda Rules!");
-		System.out.println("3rd commit");
-		System.out.println("whatever");
-		System.out.println("Yoda Rules again!");
-		System.out.println("2:01");
-		System.out.println("Heya Yoda! from laptop");
-		System.out.println("from desktop");
 	}
 }
